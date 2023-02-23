062aaa5cd1608dc24465bfc5550299ddbb743d9a
Eddie4k-code <98666468+Eddie4k-code@users.noreply.github.com>
2023-02-23 18:28:33 -0500

    Styling Change

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,40 +7,38 @@ export const Projects = () => {
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <div class="card bg-black">
-                        <div class="card-header">
-                            <h1>Web Projects</h1>
-                        </div>
-                        <div class="card-body">
+                    <div className="card bg-dark text-white mb-4">
+                        <div className="card-header"><h1>Web Projects</h1></div>
+                        <div className="card-body">
                             {projects.filter((project) => project.category === "Web").map((proj) =>
-                                <div className="bg-black border border- border-2 pt-4">
-                                    <h5 class="card-title">{proj.name}</h5>
-                                    <p class="card-text">{proj.description}</p>
-                                    <p class="card-caption"><b>Language: </b>{proj.Language}</p>
-                                    <p class="card-caption"><b>Technologies: </b>{proj.Technologies}</p>
-                                    {proj.imageUrl && <img class="card-img-top img-fluid" src={proj.imageUrl} alt="Project screenshot" />}
-                                    <a href={proj.Url} class="btn btn-primary">Source Code</a>
-                                    <br></br>
-                                    <br></br>
-                                    {proj.isLive && <a href={proj.isLive} class="btn btn-primary">View Live</a>}
+                                <div key={proj.id} className="card bg-black border border-secondary border-2 mb-3">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{proj.name}</h5>
+                                        <p className="card-text">{proj.description}</p>
+                                        <p className="card-caption"><b>Language: </b>{proj.Language}</p>
+                                        <p className="card-caption"><b>Technologies: </b>{proj.Technologies}</p>
+                                        {proj.imageUrl && <img className="card-img-top img-fluid" src={proj.imageUrl} alt="Project screenshot" />}
+                                        <a href={proj.Url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Source Code</a>
+                                        {proj.isLive && <a href={proj.isLive} className="btn btn-outline-primary ml-3" target="_blank" rel="noopener noreferrer">View Live</a>}
+                                    </div>
                                 </div>
                             )}
                         </div>
                     </div>
                 </div>
                 <div className="col-md-6">
-                    <div class="card bg-black">
-                        <div class="card-header">
-                            <h1>Test Automation Projects</h1>
-                        </div>
-                        <div class="card-body">
+                    <div className="card bg-dark text-white mb-4">
+                        <div className="card-header"><h1>Test Automation Projects</h1></div>
+                        <div className="card-body">
                             {projects.filter((project) => project.category === "Test").map((proj) =>
-                                <div className="bg-black border border- border-2 pt-4">
-                                    <h5 class="card-title">{proj.name}</h5>
-                                    <p class="card-text">{proj.description}</p>
-                                    <p class="card-caption"><b>Language: </b>{proj.Language}</p>
-                                    <p class="card-caption"><b>Technologies: </b>{proj.Technologies}</p>
-                                    <a href={proj.Url} class="btn btn-primary">Source Code</a>
+                                <div key={proj.id} className="card bg-black border border-secondary border-2 mb-3">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{proj.name}</h5>
+                                        <p className="card-text">{proj.description}</p>
+                                        <p className="card-caption"><b>Language: </b>{proj.Language}</p>
+                                        <p className="card-caption"><b>Technologies: </b>{proj.Technologies}</p>
+                                        <a href={proj.Url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Source Code</a>
+                                    </div>
                                 </div>
                             )}
                         </div>
@@ -48,5 +46,5 @@ export const Projects = () => {
                 </div>
             </div>
         </div>
-        );
+    );
 }
